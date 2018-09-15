e2be598e6ef7a96717b9d39ff4201b150bb9e8f8
Casey Calkins <c.angelcalkins@outlook.com>
2018-09-15 10:48:54 -0500

    Adding the calculator buttons with JavaScript

--- calc.js
+++ calc.js
@@ -5,8 +5,60 @@ const secondDiv = document.querySelector('#secondRow');
 const thirdDiv = document.querySelector('#thirdRow');
 const fourthDiv = document.querySelector('#fourthRow');
 
-// ----------- Appends child elements to the parent element and assigns IDs to each child div
-// ----- Appends first row of numbers/function to top row
+
+const num7 = document.createElement('button');
+firstDiv.appendChild(num7);
+num7.setAttribute('id', '7');
+
+const num8 = document.createElement('button');
+firstDiv.appendChild(num8);
+num8.setAttribute('id', '8');
+
+const num9 = document.createElement('button');
+firstDiv.appendChild(num9);
+num9.setAttribute('id', '9');
+
+const divide = document.createElement('button');
+firstDiv.appendChild(divide);
+divide.setAttribute('id', 'divide');
+
+const num4 = document.createElement('button');
+secondDiv.appendChild(num4);
+num4.setAttribute('id', '4');
+
+const num5 = document.createElement('button');
+secondDiv.appendChild(num5);
+num5.setAttribute('id', '5')
+
+const num6 = document.createElement('button');
+secondDiv.appendChild(num6);
+num6.setAttribute('id', '6');
+
+const mult = document.createElement('button');
+secondDiv.appendChild(mult);
+mult.setAttribute('id', 'multiply');
+
+const num1 = document.createElement('button');
+thirdDiv.appendChild(num1);
+num1.setAttribute('id', '1');
+
+const num2 = document.createElement('button');
+thirdDiv.appendChild(num2);
+num2.setAttribute('id', '2');
+
+const num3 = document.createElement('button');
+thirdDiv.appendChild(num3);
+num3.setAttribute('id', '3');
+
+const minus = document.createElement('button');
+thirdDiv.appendChild(minus);
+minus.setAttribute('id', 'subtract');
+
+
+
+
+
+
 
 
 
