a0736253ea28d1860fda8d3cbdedf294ed384473
Casey Calkins <c.angelcalkins@outlook.com>
2018-09-14 21:03:17 -0500

    Creating calculator basics

---
+++ calc.js
@@ -0,0 +1,17 @@
+// ------------ Creates and selects HTML elements
+const container = document.querySelector('.container');
+const firstDiv = document.querySelector('#firstRow');
+const secondDiv = document.querySelector('#secondRow');
+const thirdDiv = document.querySelector('#thirdRow');
+const fourthDiv = document.querySelector('#fourthRow');
+
+// ----------- Appends child elements to the parent element and assigns IDs to each child div
+// ----- Appends first row of numbers/function to top row
+
+
+
+// ------- Appends second row of numbers and functions to second row
+// ------- 
+
+
+
