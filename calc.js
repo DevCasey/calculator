98e866912d13ff96874ed94c4eba3e63c076a33c
Casey Calkins <c.angelcalkins@outlook.com>
2018-09-16 18:58:30 -0500

    JavaScript

--- calc.js
+++ calc.js
@@ -9,19 +9,22 @@ const display = document.querySelector('.display');
 
 
 
+let result = document.querySelector('#calculation');
+result.textContent = "0";
+
 const num7 = document.createElement('button');
 firstDiv.appendChild(num7);
-num7.setAttribute('id', '7');
+num7.setAttribute('id', 'num7');
 num7.textContent = "7";
 
 const num8 = document.createElement('button');
 firstDiv.appendChild(num8);
-num8.setAttribute('id', '8');
+num8.setAttribute('id', 'num8');
 num8.textContent = "8";
 
 const num9 = document.createElement('button');
 firstDiv.appendChild(num9);
-num9.setAttribute('id', '9');
+num9.setAttribute('id', 'num9');
 num9.textContent = "9";
 
 const divide = document.createElement('button');
@@ -31,17 +34,17 @@ divide.textContent = "/";
 
 const num4 = document.createElement('button');
 secondDiv.appendChild(num4);
-num4.setAttribute('id', '4');
+num4.setAttribute('id', 'num4');
 num4.textContent = "4";
 
 const num5 = document.createElement('button');
 secondDiv.appendChild(num5);
-num5.setAttribute('id', '5')
+num5.setAttribute('id', 'num5')
 num5.textContent = "5";
 
 const num6 = document.createElement('button');
 secondDiv.appendChild(num6);
-num6.setAttribute('id', '6');
+num6.setAttribute('id', 'num6');
 num6.textContent = "6";
 
 const mult = document.createElement('button');
@@ -51,17 +54,17 @@ mult.textContent = "*";
 
 const num1 = document.createElement('button');
 thirdDiv.appendChild(num1);
-num1.setAttribute('id', '1');
+num1.setAttribute('id', 'num1');
 num1.textContent = "1";
 
 const num2 = document.createElement('button');
 thirdDiv.appendChild(num2);
-num2.setAttribute('id', '2');
+num2.setAttribute('id', 'num2');
 num2.textContent = "2";
 
 const num3 = document.createElement('button');
 thirdDiv.appendChild(num3);
-num3.setAttribute('id', '3');
+num3.setAttribute('id', 'num3');
 num3.textContent = "3";
 
 const zero = document.createElement('button');
@@ -74,27 +77,63 @@ thirdDiv.appendChild(minus);
 minus.setAttribute('id', 'subtract');
 minus.textContent = "-";
 
+const decimal = document.createElement('button');
+fourthDiv.appendChild(decimal);
+decimal.setAttribute('id', 'decimal');
+decimal.textContent = ".";
+
+const plusOrMinus = document.createElement('button');
+fourthDiv.appendChild(plusOrMinus);
+plusOrMinus.setAttribute('id', 'plusOrMinus');
+plusOrMinus.textContent = "+/-";
+
 const add = document.createElement('button');
 fourthDiv.appendChild(add);
 add.setAttribute('id', 'add');
 add.textContent = "+";
 
-
 const clear = document.createElement('button');
-fourthDiv.appendChild(clear);
+fifthDiv.appendChild(clear);
 clear.setAttribute('id', 'clear');
 clear.textContent = "Clear";
 
+const undo = document.createElement('button');
+fifthDiv.appendChild(undo);
+undo.setAttribute('id', 'undo');
+undo.textContent = "Undo";
 
+const percent = document.createElement('button');
+fifthDiv.appendChild(percent);
+percent.setAttribute('id', 'percent');
+percent.textContent = "%";
 
+const equals = document.createElement('button');
+fifthDiv.appendChild(equals);
+equals.setAttribute('id', 'equals');
+equals.textContent = "=";
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
 
+function addition(a,b) {
+	return a+b;
+}
 
+function subtraction(a,b) {
+	return a-b;
+}
 
+function multiplication(a,b) {
+	return a*b;
+}
 
+function division(a,b) {
+}
 
+function screenClear() {
+	calculation.textContent = "0";
+}
 
-// ------- Appends second row of numbers and functions to second row
-// ------- 
-
-
+function operate(operator, num1, num2) {
+	
+}
 
